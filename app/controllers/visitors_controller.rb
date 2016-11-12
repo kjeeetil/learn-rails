b44f680b22b03d56df853edc14ab1040a0f29fd1
kjeeetil <kjetil.mo@gmail.com>
2016-11-12 16:29:22 +0100

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,13 @@
 class VisitorsController < ApplicationController
 
 	def new
+		Rails.logger.debug 'DEBUG: entering new method'
 		@owner = Owner.new
+		Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
+		flash[:notice] = 'Wilkommen!'
+		flash[:alert] = 'Bursdagen min er nært forestående'
 	end
 end
+
+
+#jeg er på side 132
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,13 +1,14 @@
 class Owner
 	def name
-		name ="kjetil"
+		name ="Kjetil"
 	end
 
 	def birthdate
-		birthdate = Date.new(1986,12,22)
+		birthdate = Date.new(1986,07,22)
 	end
 
 	def countdown
+		Rails.logger.debug "\033[1;34;40mDEBUG: \033[0m " + 'entering Owner countdown method'
 		today = Date.today
 		birthday = Date.new(today.year, birthdate.month, birthdate.day)
 		if birthday > today
