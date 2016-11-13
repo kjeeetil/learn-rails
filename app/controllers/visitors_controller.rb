311e9d1ebec1422ee58d3bc319356dc94214adad
kjeeetil <kjetil.mo@gmail.com>
2016-11-13 11:29:50 +0100

    front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,13 +1,6 @@
 class VisitorsController < ApplicationController
 
 	def new
-		Rails.logger.debug 'DEBUG: entering new method'
 		@owner = Owner.new
-		Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
-		flash[:notice] = 'Wilkommen!'
-		flash[:alert] = 'Bursdagen min er nært forestående'
 	end
 end
-
-
-#jeg er på side 132
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -4,17 +4,17 @@ class Owner
 	end
 
 	def birthdate
-		birthdate = Date.new(1986,07,22)
+		birthdate = Date.new(2016,11,8)
 	end
 
 	def countdown
 		Rails.logger.debug "\033[1;34;40mDEBUG: \033[0m " + 'entering Owner countdown method'
 		today = Date.today
-		birthday = Date.new(today.year, birthdate.month, birthdate.day)
+		birthday = Date.new(2020, 11, 8)
 		if birthday > today
 			countdown = (birthday - today). to_i
 		else
-			countdown = (birthday.next_year - today).to_i
+			countdown = (birthday - today).to_i
 		end
 	end
 end
